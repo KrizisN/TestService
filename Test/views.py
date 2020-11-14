ec1ff29fcf0c940f52a13ee5777b7134d76ffe12
KrizisN <60068209+KrizisN@users.noreply.github.com>
2020-11-15 01:23:11 +0200

    Удалил не нужный импорт

--- Test/views.py
+++ Test/views.py
@@ -4,7 +4,6 @@ from .form import *
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import Group
 from .decorators import *
 
 
